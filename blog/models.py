04320404e2e4b5bccc2bc0046e2b2e6e209b6e09
xuting1108 <xuting1108@hotmail.com>
2019-08-27 19:36:29 -0300

    Fix import

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models 
-from django.uils import timezone
+from django.utils import timezone
 
 class Post (models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
